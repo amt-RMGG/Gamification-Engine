0e2c5ff1bb50bb741483ec269fcc878cc1a74ee3
Gildas <gildas.houlmann@gmail.com>
2021-01-09 17:07:51 +0100

    cucumber tests compile again

--- badges-specs/src/test/java/amt/rmgg/gamification/api/spec/steps/EventSteps.java
+++ badges-specs/src/test/java/amt/rmgg/gamification/api/spec/steps/EventSteps.java
@@ -1,32 +1,32 @@
-//package amt.rmgg.gamification.api.spec.steps;
-//
-//import amt.rmgg.gamification.ApiException;
-//import amt.rmgg.gamification.ApiResponse;
-//import amt.rmgg.gamification.api.DefaultApi;
-//import amt.rmgg.gamification.api.dto.Event;
-//import amt.rmgg.gamification.api.spec.helpers.Environment;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.When;
-//
-//import java.util.List;
-//public class EventSteps {
-//
-//    Event event;
-//
-//    @Given("I have a event payload")
-//    public void i_have_a_event_payload() throws Throwable {
-//        event = new Event()
-//                .name("firstUpvote")
-//                .userid(1);
-//    }
-//
-//    @When("^I POST the event payload to the /events endpoint$")
-//    public void i_POST_the_event_payload_to_the_events_endpoint() {
-//        try {
-//            StepsHelper.lastApiResponse = StepsHelper.api.sendEventWithHttpInfo(event);
-//            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
-//        } catch (ApiException e) {
-//            StepsHelper.processApiException(e);
-//        }
-//    }
-//}
+package amt.rmgg.gamification.api.spec.steps;
+
+import amt.rmgg.gamification.ApiException;
+import amt.rmgg.gamification.ApiResponse;
+import amt.rmgg.gamification.api.DefaultApi;
+import amt.rmgg.gamification.api.dto.Event;
+import amt.rmgg.gamification.api.spec.helpers.Environment;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+public class EventSteps {
+
+    Event event;
+
+    @Given("I have a event payload")
+    public void i_have_a_event_payload() throws Throwable {
+        event = new Event()
+                .username("firstUpvote")
+                .eventTypeId((long)1);
+    }
+
+    @When("^I POST the event payload to the /events endpoint$")
+    public void i_POST_the_event_payload_to_the_events_endpoint() {
+        try {
+            StepsHelper.lastApiResponse = StepsHelper.api.sendEventWithHttpInfo(event);
+            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
+        } catch (ApiException e) {
+            StepsHelper.processApiException(e);
+        }
+    }
+}
--- badges-specs/src/test/java/amt/rmgg/gamification/api/spec/steps/RuleSteps.java
+++ badges-specs/src/test/java/amt/rmgg/gamification/api/spec/steps/RuleSteps.java
@@ -1,61 +1,61 @@
-//package amt.rmgg.gamification.api.spec.steps;
-//
-//import amt.rmgg.gamification.ApiException;
-//import amt.rmgg.gamification.api.dto.Badge;
-//import amt.rmgg.gamification.api.dto.Rule;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class RuleSteps {
-//
-//    Rule rule;
-//    Rule lastReceivedRule;
-//
-//    @Given("i have a rule payload")
-//    public void iHaveARulePayload() {
-//        rule = new amt.rmgg.gamification.api.dto.Rule()
-//                .badgeId(1)
-//                .eventType("upvote")
-//                .threshold(100);
-//    }
-//
-//    @When("^I POST the rule payload to the /rules endpoint$")
-//    public void I_POST_the_rule_payload_to_the_rules_endpoint() {
-//        try {
-//            StepsHelper.lastApiResponse = StepsHelper.api.createRuleWithHttpInfo(rule);
-//            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
-//            lastReceivedRule = (Rule)StepsHelper.lastApiResponse.getData();
-//        } catch (ApiException e) {
-//            StepsHelper.processApiException(e);
-//        }
-//    }
-//
-//    @Then("I receive a rule that is the same as the payload")
-//    public void iReceiveARuleThatIsTheSameAsThePayload() {
-//        assertEquals(lastReceivedRule, rule);
-//    }
-//
-//    @When("^I send a GET to the /rules endpoint$")
-//    public void iSendAGETToTheRulesEndpoint() {
-//        try {
-//            StepsHelper.lastApiResponse = StepsHelper.api.getRulesWithHttpInfo();
-//            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
-//        } catch (ApiException e) {
-//            StepsHelper.processApiException(e);
-//        }
-//    }
-//
-//    @Then("I GET a rule with ID {int}")
-//    public void iGETARuleWithID(int id) {
-//        try {
-//            StepsHelper.lastApiResponse = StepsHelper.api.getRuleWithHttpInfo(id);
-//            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
-//        } catch (ApiException e) {
-//            StepsHelper.processApiException(e);
-//        }
-//    }
-//
-//}
+package amt.rmgg.gamification.api.spec.steps;
+
+import amt.rmgg.gamification.ApiException;
+import amt.rmgg.gamification.api.dto.Badge;
+import amt.rmgg.gamification.api.dto.Rule;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+public class RuleSteps {
+
+    Rule rule;
+    Rule lastReceivedRule;
+
+    @Given("i have a rule payload")
+    public void iHaveARulePayload() {
+        rule = new amt.rmgg.gamification.api.dto.Rule()
+                .badgeId(1)
+                .eventTypeId((long)1)
+                .threshold(100);
+    }
+
+    @When("^I POST the rule payload to the /rules endpoint$")
+    public void I_POST_the_rule_payload_to_the_rules_endpoint() {
+        try {
+            StepsHelper.lastApiResponse = StepsHelper.api.createRuleWithHttpInfo(rule);
+            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
+            lastReceivedRule = (Rule)StepsHelper.lastApiResponse.getData();
+        } catch (ApiException e) {
+            StepsHelper.processApiException(e);
+        }
+    }
+
+    @Then("I receive a rule that is the same as the payload")
+    public void iReceiveARuleThatIsTheSameAsThePayload() {
+        assertEquals(lastReceivedRule, rule);
+    }
+
+    @When("^I send a GET to the /rules endpoint$")
+    public void iSendAGETToTheRulesEndpoint() {
+        try {
+            StepsHelper.lastApiResponse = StepsHelper.api.getRulesWithHttpInfo();
+            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
+        } catch (ApiException e) {
+            StepsHelper.processApiException(e);
+        }
+    }
+
+    @Then("I GET a rule with ID {int}")
+    public void iGETARuleWithID(int id) {
+        try {
+            StepsHelper.lastApiResponse = StepsHelper.api.getRuleWithHttpInfo(id);
+            StepsHelper.processApiResponse(StepsHelper.lastApiResponse);
+        } catch (ApiException e) {
+            StepsHelper.processApiException(e);
+        }
+    }
+
+}
